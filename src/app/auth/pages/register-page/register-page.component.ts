3ef625936bb35f86a49ecd40d0d61d91686d9d47
David <alv.ricardodavid@gmail.com>
2024-07-28 18:45:59 -0300

    Validaciones Asincronas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
-  selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        ,
+        Validators.pattern(customValidators.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(customValidators.emailPattern)],
+    ],
+    username: ['', [Validators.required, customValidators.canBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    //TODO: Objtener el valor de un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+
+    console.warn(this.myForm.value);
+  }
 }
